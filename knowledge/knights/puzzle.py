ce2accba7b9f9ba4d65bc0385ea344f30bc256f9
Jean-Javid Nobari <j.nobari@yahoo.com>
2025-08-12 09:01:29 +0200

    wip

--- knowledge/knights/puzzle.py
+++ knowledge/knights/puzzle.py
@@ -12,21 +12,52 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    # A cannot be both a knight and a knave
+    Not(And(AKnight, AKnave)),
+    
+    # If A is a knight, then the statement must be true
+    Implication(AKnight, And(AKnight, AKnave)),
+    
+    # If A is a knave, then the statement must be false
+    Implication(AKnave, Not(And(AKnight, AKnave)))
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    # A and B cannot be both Knaves
+    Not(And(AKnave, BKnave)),
+
+    # If A is a knight, then the statement must be true
+    Implication(AKnight, And(AKnave, BKnave)),
+
+    # If A is a knave, then the statement must be false
+    Implication(AKnave, Not(And(AKnave, BKnave)))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+
+    # A and B cannot be both Knights
+    Not(And(AKnight, BKnight)),
+
+    # A and B cannot be both Knaves
+    Not(And(AKnave, BKnave)),
+
+    # If A is a knight, then the statement must be true
+    Implication(AKnight, And(AKnave, BKnave)),
+
+    # If B is a knight, then the statement must be true
+    Implication(BKnight, And(AKnave, BKnave)),
+
+    # If A is a knave, then the statement must be false
+    Implication(AKnave, Not(And(AKnave, BKnave))),
+
+    # If B is a knave, then the statement must be false
+    Implication(BKnave, Not(And(AKnight, BKnave)))
 )
 
 # Puzzle 3
@@ -35,7 +66,7 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    
 )
 
 
